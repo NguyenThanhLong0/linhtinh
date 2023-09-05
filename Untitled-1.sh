2efbd9036fdbb7394a75c2a2f6add6e43dcec8a3
LongDino <longntph45139@fpt.edu.vn>
2023-09-05 18:45:01 +0700

    learn git1

---
+++ Untitled-1.sh
@@ -0,0 +1,7 @@
+$ git config --global user.name <username>
+$ git config --global user.email <mailaddress>
+ssh-keygen -t rsa -b 4096 -C "email_cua_ban@example.com" 
+ssh-add ~/.ssh/id_rsa
+eval "$(ssh-agent -s)"
+# Agent pid 59566
+C:\Users\nguye\OneDrive\Tài liệucd nguyen
