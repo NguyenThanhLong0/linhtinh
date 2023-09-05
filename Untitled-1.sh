0cada5aaf336328dff5ffdb36cb441e89683d044
LongDino <longntph45139@fpt.edu.vn>
2023-09-05 18:54:23 +0700

    test

--- Untitled-1.sh
+++ Untitled-1.sh
@@ -4,4 +4,4 @@ ssh-keygen -t rsa -b 4096 -C "email_cua_ban@example.com"
 ssh-add ~/.ssh/id_rsa
 eval "$(ssh-agent -s)"
 # Agent pid 59566
-C:\Users\nguye\OneDrive\Tài liệucd nguyen
+C:\Users\nguye\OneDrive\Tàiliệu
